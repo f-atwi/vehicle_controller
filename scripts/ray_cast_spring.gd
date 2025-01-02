1c97d96b08f03f11debef36d100826d2edbd6d40
Fouad Atwi <atwe98@gmail.com>
2025-01-02 20:10:59 +0100

    fix length clamp

--- scripts/ray_cast_spring.gd
+++ scripts/ray_cast_spring.gd
@@ -5,7 +5,7 @@ extends RayCast3D
 @export var x_preload_fraction: float = 0.5 # Range .5 to .75
 @export var spring_natural_length: float = 0.4 # Range .25 to .50
 @export var wheel_radius: float = 0.2 # Range .18m to .25m  
-@export var damping_coefficient = 10000 # should be studied for its value
+@export var damping_coefficient = 1 # should be studied for its value
 
 var previous_length: float
 var stiffness: float
@@ -22,7 +22,6 @@ func _ready() -> void:
 	x_equilibrium = ProjectSettings.get_setting("physics/3d/default_gravity") / pow(TAU * car_frequency, 2)
 	spring_length = spring_natural_length - x_preload_fraction*x_equilibrium
 	target_position.y = -spring_length - wheel_radius # must be adjusted with wheel size
-	x_preloaded = spring_natural_length * x_preload_fraction
 	# Check that equilibium is less that spring length 
 	assert (x_equilibrium < spring_natural_length)
 	assert (x_equilibrium < spring_length)
@@ -30,7 +29,7 @@ func _ready() -> void:
 	
 func _physics_process(delta: float) -> void:
 	if is_colliding():
-		var length: float = clamp(get_collision_point().distance_to(global_position) - wheel_radius, wheel_radius, spring_length)	
+		var length: float = clamp(get_collision_point().distance_to(global_position) - wheel_radius, 0, spring_length)	
 		var delta_x := spring_natural_length - length
 		var speed = (previous_length - length) / delta
 		rigid_body_3d.apply_force(delta_x*(stiffness+speed*damping_coefficient)*transform.basis.y, global_position)
